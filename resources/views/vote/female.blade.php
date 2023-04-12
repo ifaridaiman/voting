dd102e528ad3d5bbb402b118171bdc50fbc7a298
ifaridaiman <ifaridaiman@gmail.com>
2023-04-12 17:02:46 +0800

    fix it

--- resources/views/vote/female.blade.php
+++ resources/views/vote/female.blade.php
@@ -6,15 +6,15 @@
 </div>
 <div class="grid grid-cols-2 gap-4 p-4">
     @foreach($candidate as $key => $value)
-        <div class="bg-white w-36 h-56 mx-auto">
+        <div class="bg-white w-36 h-56 mx-auto overflow-hidden">
+
             <form id="vote-form" method="POST" action="{{ route('vote.process') }}">
                 @csrf
                 <input type="hidden" name="candidate_id" value="{{ $value->id }}">
                 <input type="hidden" name="user_id" value="{{ $user_id}}">
-
                 <button class="w-full mx-auto p-2 text-blue-300 hover:text-blue-500 font-bold relative" type="button" onclick="confirmVote()">
                     <span>Vote Me</span>
-                    <img class="absolute top-0 left-0 h-full w-full object-contain" src="{{ $value->img_path }}"/>
+                    <img class="w-full h-full object-fit-contain" src="{{ $value->img_path }}"/>
                 </button>
             </form>
         </div>
